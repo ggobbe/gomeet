9afec829c1419ce02fd40f27657ac0e0393d167e
Martin Kotula <kotula.mar@gmail.com>
2015-01-24 13:07:32 +0000

    Helper methods in User. Usage of regocommend

--- recommendation/recommendation.go
+++ recommendation/recommendation.go
@@ -1,10 +1,15 @@
 package recommendation
 
-import "gomeet/user"
+import (
+	"gomeet/user"
+	"log"
+
+	"github.com/muesli/regommend"
+)
 
 type Recommendation struct {
 	User  *user.User
-	Ratio float64
+	Score float64
 }
 
 type Recommender interface {
@@ -17,7 +22,35 @@ type SimpleRecommender struct {
 
 func (sr SimpleRecommender) GetRecommendations(user *user.User) ([]Recommendation, error) {
 
-	// := sr.uRepo.GetUsers()
+	users, err := sr.uRepo.GetUsers()
+	if err != nil {
+		return nil, err
+	}
+
+	interests := regommend.Table("interests")
+	for _, u := range users {
+		interests.Add(u.Name, u.Interests.AsMap())
+	}
+	interests.Add(user.Name, user.Interests.AsMap())
+
+	log.Print("Table ", interests)
+	log.Println("Looking for recommendations for user ", user.Name)
+	recs, err := interests.Recommend(user.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, rec := range recs {
+		log.Println("Recommending", rec.Key, "with score", rec.Distance)
+	}
+
+	neighbours, err := interests.Neighbors("Chris")
+	if err != nil {
+		return nil, err
+	}
+	for _, rec := range neighbours {
+		log.Println("Neighbours", rec.Key, "with score", rec.Distance)
+	}
 
 	return make([]Recommendation, 0), nil
 }
--- user/user.go
+++ user/user.go
@@ -3,13 +3,15 @@ package user
 type User struct {
 	Name      string
 	Location  Location
-	Interests []*Interest
+	Interests UserInterests
 }
 
 type Location struct {
 	Longitute float64
 	Latitude  float64
 }
+
+type UserInterests []*Interest
 
 type Interest struct {
 	Name   string
@@ -26,4 +28,13 @@ func NewUser(name string, interests []*Interest) *User {
 
 func NewInterest(name string, rating float64) *Interest {
 	return &Interest{Name: name, Rating: rating}
+}
+
+func (ui UserInterests) AsMap() map[interface{}]float64 {
+	interestMap := make(map[interface{}]float64)
+
+	for _, i := range ui {
+		interestMap[i.Name] = i.Rating
+	}
+	return interestMap
 }
