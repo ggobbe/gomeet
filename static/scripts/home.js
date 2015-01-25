70fbb850f4acf31589ea23014f712ce58edd6c6a
Martin Kotula <kotula.mar@gmail.com>
2015-01-25 12:49:39 +0000

    Home - display user recommendations on the page

---
+++ static/scripts/home.js
@@ -0,0 +1,39 @@
+
+
+$(function() {
+  function initialize() {
+
+    var mapOptions = {
+      center: user.location,
+      zoom: 10
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var marker = new google.maps.Marker({
+      position: user.location,
+      map: map,
+      title:user.name
+    });
+
+    var removeAnimation = $.noop
+    $.each(users, function(index, user){
+      var marker = new google.maps.Marker({
+        position: user.location,
+        map: map,
+        title: user.name,
+        animation: google.maps.Animation.DROP
+      });
+
+      $("#usr-zoom-" + user.name).click(function(e){
+        removeAnimation()
+        $("h2")[0].scrollIntoView();
+        map.setCenter(user.location);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        removeAnimation = function(){ marker.setAnimation(null); }
+        e.preventDefault();
+      })
+    });
+  };
+
+  initialize();
+})
