d2c81ac8022267131718f4ce0c29f17a3f978d20
ggobbe <guillaume.gobbe@gmail.com>
2015-01-25 11:12:42 +0000

    MongoDB user repository

---
+++ user/MgoRepo.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"edigophers/utils"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// MgoRepo is a repository based on a MongoDb
+type MgoRepo struct {
+	url        string
+	database   string
+	collection string
+}
+
+func (r MgoRepo) getUserCollection() *mgo.Collection {
+	s, err := mgo.Dial(r.url)
+	utils.CheckError(err)
+	return s.DB(r.database).C(r.collection)
+}
+
+// GetUser gets a user per his username
+func (r MgoRepo) GetUser(name string) (*User, error) {
+	c := r.getUserCollection()
+	user := User{}
+	err := c.Find(bson.M{"name": name}).One(&user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+//GetUsers is a function returning a list of users
+func (r MgoRepo) GetUsers() ([]User, error) {
+	c := r.getUserCollection()
+	users := []User{}
+	err := c.Find(nil).All(&users)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
